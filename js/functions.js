428ed0714ae68a7325ffb47382b6c36d971f6b01
DavidCedano <38667472+DavidCedano@users.noreply.github.com>
2018-05-19 15:25:09 -0600

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -231,7 +231,7 @@ function queryFormSuccess(tx, results) {
 		$("#alta_domicilio").val($.registro.domicilio);
 		
 		
-		$("#opciones").selected("#cat_"+$.registro.categoria);//trigger("click").trigger("click");
+		$("#opciones").option("#cat_"+$.registro.categoria).selected;//trigger("click").trigger("click");
 			//$("#cat_"+$.registro.categoria).attr("checked",true).checkboxradio("refresh");
 		$("#alta_nota").val($.registro.nota);
 }
