bb2b06a716790f2ef4744eb50985b43501be674b
DavidCedano <38667472+DavidCedano@users.noreply.github.com>
2018-05-18 20:11:32 -0600

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,7 @@ function mkLog(text){
 }
 
 
+
 /* 
 * variables de la aplicación
 */
@@ -25,52 +26,42 @@ function mkLog(text){
 * carga inicial de la app
 */
 function onBodyLoad() {    
-	document.addEventListener("deviceready", onDeviceReady(), false);
-	//alert('LLEGUE AL ONBODYLOAD()');
+	document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady(){
-	//alert('LLEGUE AL onDeviceReady()');
 	mkLog("Aplicación cargada y lista");
-	//alert('Aplicación cargada y lista');
-    //navigator.notification.alert('PhoneGap is working','','','');
-
-    
+    //navigator.notification.alert("PhoneGap is working");
 	
 	existe_db = window.localStorage.getItem("existe_db");
-	alert(existe_db);
-	db = window.openDatabase("servicios", "1.0", "Catalogo de servicios", 200000);
+	db = window.openDatabase("servicios", "1.0", "DB del curso Phonegap", 200000);
 	if(existe_db == null){
-		alert('CREAR BASE DE DATOS');
 		creaDB();
-		
 	}else{
-		alert('CARGAR LA BASE DE DATOS');
 		cargaDatos();
 	}
 	
-	$('#btn_guardar').click(function(e){
+	
+	$("#btn_guardar").click(function(e){
 		if($.id != -1){
-			alert('SAVE EDIT FORM');
 		 	saveEditForm();
 		 }else{
-		 	alert('SAVE NEW FORM');
 			saveNewForm();
 		 }
 	 });
 }
 
+
 /* 
-* creación de la base de datos
+* creación de ña base de datos
 */
 function creaDB(){
-	db.transaction(creaNuevaDB, errorDB, creaSuccess());
+	db.transaction(creaNuevaDB, errorDB, creaSuccess);
 	
 }
 
 function creaNuevaDB(tx){
 	mkLog("Creando base de datos");
-	alert('Creando base de datos');
 	
 	tx.executeSql('DROP TABLE IF EXISTS servicios');
 	
@@ -80,77 +71,33 @@ function creaNuevaDB(tx){
 		"imagen VARCHAR(50), " +
 		"telefono VARCHAR(30), " +
 		"email VARCHAR(30), " +
-		"domicilio VARCHAR(30), " +
-		"categoria VARCHAR(30), " +  
-		"nota VARCHAR(50) )";
+		"domicilio VARCHAR(200), " +
+		"categoria VARCHAR(200), " + 
+		"nota VARCHAR(30) )";
 		
-	alert('Se creo la base de datos');
-
 	tx.executeSql(sql);
 	
-	tx.executeSql("INSERT INTO servicios (nombre,imagen,telefono,email,domicilio,categoria,nota) VALUES ('Ejemplo1','imagen1','6699900970','ejemplo@mail.com','granadas','lista_administrador','soy el mejor yeah')");
-
-	alert('YA INSERTE');
+	tx.executeSql("INSERT INTO servicios (id,nombre,imagen,telefono,email,domiclio,categoria,nota) VALUES (1,'Ejemplo1','','+6699900970','ejemplo@mail.com','granadas 258','Carpíntero','soy el mejor yeah!')");
 }
+
 
 function creaSuccess(){
 	window.localStorage.setItem("existe_db", 1);
-	alert('VOY A CARGAR datos: ');
 	cargaDatos();
 }
 
 function errorDB(err){
 	mkLog("Error procesando SQL " + err.code);
-	alert('Error procesando SQL: ' + err.code);
-	//navigator.notification.alert("Error procesando SQL " + err.code);
-	//alert('Error procesando SQL');
-
+	navigator.notification.alert("Error procesando SQL " + err.code);
 }
 
-/*
-* creando registros
-*/
-function saveNewForm(){
-	if(db != null){
-		db.transaction(queryDBInsertForm, errorDB);
-	}
 
-	//$.mobile.changePage("#inicio");
-}
-
-function queryDBInsertForm(tx){
-	var cat = $("#lista_categoria").find("input:selected").val();
-	
-	tx.executeSql("INSERT INTO servicios (nombre,imagen,telefono,email,domicilio,categoria,nota) VALUES ('"+$("#nombre").val()+"','"+$("#imagen").val()+"','"+$("#telefono").val()+"','"+$("#email").val()+"','"+$("#domicilio").val()+"','"+cat+"','"+$("#nota").val()+"')", [], newFormSuccess, errorDB);
-	lert('INSERTE DE NUEVO');
-}
-function newFormSuccess(tx, results) {
-	var cat = $("#lista_categoria").find("input:selected").val();
-	var lista = $("#lista_" + cat + " ul")
-	
-	// <img src="'+ $.imageUR +'" class="img_peq"/> esto va antes de nombre
-	
-	var obj = $('<li id="li_'+results.insertId+'"><a href="#detalle" data-uid='+results.insertId+' class="linkDetalles"><div class="interior_lista"><span>' + $("#nombre").val() + " " + $("#imagen").val()+ '</span></div></a><a href="#form"  data-theme="a" data-uid='+results.insertId+'  class="linkForm">Predet.</a></li>');
-	obj.find('.linkDetalles').bind('click', function(e){
-		$.id = $(this).data('uid');
-	});
-	
-	obj.find('.linkForm').bind('click', function(e){
-		$.id = $(this).data('uid');
-	});
-	lista.append(obj).listview('refresh');
-	
-	
-	$.mobile.changePage("#inicio");
-}
 
 /* 
 * carga de datos desde la base de datos
 */
 function cargaDatos(){
 	db.transaction(cargaRegistros, errorDB);
-	alert('ya carge datos');
-	
 }
 
 function cargaRegistros(tx){
@@ -170,9 +117,9 @@ function cargaDatosSuccess(tx, results){
 		var selector = $("#lista_" + persona.categoria + " ul");
 		var foto = persona.foto;
 		if(foto == ""){
-			//foto = "assets/no_foto.png";
+			foto = "assets/no_foto.png";
 		}
-		selector.append('<li id="li_'+persona.id+'"><a href="#detalle" data-uid='+persona.id+' class="linkDetalles"><div class="interior_lista"><img src="'+ foto +'" class="img_peq"/><span>' + persona.nombre + ' ' + persona.imagen + '</span></div></a><a href="#form"  data-theme="a" data-uid='+persona.id+'  class="linkForm">Predet.</a></li>').listview('refresh');
+		selector.append('<li id="li_'+persona.id+'"><a href="#detalle" data-uid='+persona.id+' class="linkDetalles"><div class="interior_lista"><img src="'+ foto +'" class="img_peq"/><span>' + persona.nombre + ' ' + persona.apellidos+ '</span></div></a><a href="#AgregarServicio"  data-theme="a" data-uid='+persona.id+'  class="linkForm">Predet.</a></li>').listview('refresh');
 	}
 	
 	$(".linkDetalles").click(function(e){
@@ -184,3 +131,165 @@ function cargaDatosSuccess(tx, results){
 	});
 }
 
+
+
+
+/*
+* vista detalle
+*/
+
+$(document).on("pagebeforeshow", "#detalle", function(){
+	if(db != null){
+		db.transaction(queryDBFindByID, errorDB);
+	}
+});
+
+
+
+function queryDBFindByID(tx) {
+    tx.executeSql('SELECT * FROM servicios WHERE id='+$.id, [], queryDetalleSuccess, errorDB);
+}
+
+function queryDetalleSuccess(tx, results) {
+	mkLog("Recibidos de la DB en vista detalle" + results.rows.length + " registros");
+	if(results.rows.length == 0){
+		mkLog("No se han recibido registros para la vista detalle");
+		navigator.notification.alert("No hay detalles para ese elemento");
+	}
+	
+	$.registro = results.rows.item(0);
+	$("#categoria").html($.registro.categoria);
+		var _foto = $.registro.foto;
+		if(_foto == ""){
+			_foto = "assets/no_foto.png";
+		}
+		$("#foto_img").attr("src", _foto);
+		$("#nombre").html($.registro.nombre + " " + $.registro.apellidos);
+		$("#num_tel").html($.registro.telefono);
+		$("#telefono").attr("href", "tel:" + $.registro.telefono);
+		$("#label_mail").html("Mail: " + $.registro.email);
+}
+
+
+
+
+
+/*
+* vista detalle
+*/
+//vista de la página de edición
+$(document).on('pagebeforeshow', '#AgregarServicio', function(){ 
+	mkLog('ID recuperado en vista form: ' + $.id);
+	
+	initForm();
+	if(db != null && $.id != -1){
+		db.transaction(queryDBFindByIDForm, errorDB);
+	}
+});
+
+function queryDBFindByIDForm(tx) {
+    tx.executeSql('SELECT * FROM servicios WHERE id='+$.id, [], queryFormSuccess, errorDB);
+}
+
+function queryFormSuccess(tx, results) {
+	mkLog("Recibidos de la DB en vista Form" + results.rows.length + " registros");
+	if(results.rows.length == 0){
+		mkLog("No se han recibido registros para la vista form");
+		navigator.notification.alert("No hay detalles para ese elemento");
+	}
+	
+	$.registro = results.rows.item(0);
+	
+		$.imageURL = $.registro.foto;
+		if($.imageURL == ""){
+			$.imageURL = "assets/no_foto.png";
+		}
+		$("#fotoEdit_img").attr("src", $.imageURL);
+		$("#ti_nombre").val($.registro.nombre);
+		$("#ti_apellidos").val($.registro.apellidos);
+		$("#ti_telefono").val($.registro.telefono);
+		$("#ti_mail").val($.registro.email);
+		
+		$("#cat_"+$.registro.categoria).trigger("click").trigger("click");	//$("#cat_"+$.registro.categoria).attr("checked",true).checkboxradio("refresh");
+}
+$(document).on('pagebeforeshow', '#inicio', function(){ 
+	$.id = -1;
+});
+function initForm(){
+	$.imageURL = "assets/no_foto.png";
+	
+	$("#fotoEdit_img").attr("src", $.imageURL);
+	$("#ti_nombre").val("");
+	$("#ti_apellidos").val("");
+	$("#ti_telefono").val("");
+	$("#ti_mail").val("");
+		
+	$("#cat_familia").trigger("click").trigger("click")
+}
+
+
+/*
+* modificando registros
+*/
+function saveEditForm(){
+	if(db != null){
+		db.transaction(queryDBUpdateForm, errorDB, updateFormSuccess);
+	}
+}
+
+function queryDBUpdateForm(tx){
+	var cat = $("#cajaCategorias").find("input:checked").val();
+	tx.executeSql('UPDATE agenda_curso SET nombre="'+$("#ti_nombre").val()+'", apellidos="'+$("#ti_apellidos").val()+'",telefono="'+$("#ti_telefono").val()+'",email="'+$("#ti_mail").val()+'",categoria="'+cat+'",foto = "'+$.imageURL+'" WHERE id='+$.id);
+}
+function updateFormSuccess(tx) {
+	var selector = $("#li_"+$.id);
+	
+	var selector = $("#li_"+$.id).clone(true);
+	selector.find("img").attr("src", $.imageURL);
+	selector.find("a:first").find("span").html($("#ti_nombre").val() + " " + $("#ti_apellidos").val());
+	
+	
+	$("#li_"+$.id).remove();
+	
+	var cat = $("#cajaCategorias").find("input:checked").val();
+	var lista = $("#lista_" + cat + " ul")
+	lista.append(selector).listview('refresh');
+	
+	
+	$.mobile.changePage("#home");
+}
+
+
+
+/*
+* creando registros
+*/
+function saveNewForm(){
+	if(db != null){
+		db.transaction(queryDBInsertForm, errorDB);
+	}
+}
+
+function queryDBInsertForm(tx){
+	var cat = $("#cajaCategorias").find("input:checked").val();
+	
+	tx.executeSql("INSERT INTO agenda_curso (nombre,apellidos,telefono,categoria,foto,email) VALUES ('"+$("#ti_nombre").val()+"','"+$("#ti_apellidos").val()+"','"+$("#ti_telefono").val()+"','"+cat+"','"+$.imageURL+"','"+$("#ti_mail").val()+"')", [], newFormSuccess, errorDB);
+}
+function newFormSuccess(tx, results) {
+	var cat = $("#cajaCategorias").find("input:checked").val();
+	var lista = $("#lista_" + cat + " ul")
+	
+	
+	var obj = $('<li id="li_'+results.insertId+'"><a href="#detalle" data-uid='+results.insertId+' class="linkDetalles"><div class="interior_lista"><img src="'+ $.imageUR +'" class="img_peq"/><span>' + $("#ti_nombre").val() + " " + $("#ti_apellidos").val()+ '</span></div></a><a href="#AgregarServicio"  data-theme="a" data-uid='+results.insertId+'  class="linkForm">Predet.</a></li>');
+	obj.find('.linkDetalles').bind('click', function(e){
+		$.id = $(this).data('uid');
+	});
+	
+	obj.find('.linkForm').bind('click', function(e){
+		$.id = $(this).data('uid');
+	});
+	lista.append(obj).listview('refresh');
+	
+	
+	$.mobile.changePage("#inicio");
+}
